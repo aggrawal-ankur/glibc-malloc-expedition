cad1b50c1ed036b3604ae7bb5c620ba07065d26d
hi-anki <managemee@protonmail.com>
2026-05-15 12:58:10 +0530

    Some improvements.

--- glibc-malloc/dynamic-analysis-code/bins/exp7.c
+++ glibc-malloc/dynamic-analysis-code/bins/exp7.c
@@ -1,12 +1,12 @@
 /* EXPERIMENT 7 */
 
-/* OBJECTIVE: A largebin is basically a collection of smallbin size classes. */
+/* OBJECTIVE: A largebin is basically a collection of fixed size classes. */
 
 /* GIVEN: 
 
   1. We will use the first largebin in category #1.
   2. This bins spans across 64 bytes from the base size of 1009 bytes (until 1072), managing chunks in 1009-1072 bytes range.
-  3. The number of smallbin size classes the bins in this largebin category can fit are 4 (BIN_WIDTH/SMALLBIN_WIDTH, i.e. 64/16). For this bin, the sizes are 1024 (1008+(16*1)), 1040 (1008+(16*2)), 1056 (1008+(16*3)), 1072 (1008+(16*4)).
+  3. The number of fixed size classes the bins in this largebin category can contain are 4 (BIN_WIDTH/SMALLBIN_WIDTH, i.e. 64/16). They are 1024 (1008+(16*1)), 1040 (1008+(16*2)), 1056 (1008+(16*3)), 1072 (1008+(16*4)).
   4. Remember, these are request2size(sz) values, not the sz itself.
 
 */
@@ -37,9 +37,9 @@ The first largebin is represented by <main_arena+1016>. Since the bin is non-emp
 
 We can notice a difference in the head/tail pointers, which indicates the presence of multiple chunks in the list.
 
-Take the head address and dereference it. Take the address in fd and dereference it. Do this until we reach the address that corresponds to the header of this bin.
+Take the head address and dereference it. Take the address in fd and dereference it. Repeat until we reach the header of this bin.
 
-Read fd/bk values to form the unordered links b/w the chunks.
+Because all the freed chunks are found in a single bin, that proves that each largebin is basically a collection of fixed size classes.
 
 */
 
